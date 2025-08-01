960ff95a7aa052e1d5f4bd256a44d81f3484dae9
benhoch2 <45126030+benhoch2@users.noreply.github.com>
2025-08-01 17:33:34 +0300

    ghost spawner works

---
+++ Assets/GhostSpawner.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using Meta.XR.MRUtilityKit;
+
+public class GhostSpawner : MonoBehaviour
+{
+    public float spawnTimer = 1f;
+    public GameObject ghostPrefab;
+
+    public float minEdgeDistance = 0.3f;
+    public MRUKAnchor.SceneLabels spwanLabels;
+    public float normalOffset = 0.1f; // Offset slightly above the surface
+
+    public int spawnTry = 100;
+
+    private float timer = 0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!MRUK.Instance && !MRUK.Instance.IsInitialized)
+        {
+            return;
+        }
+
+        timer += Time.deltaTime;
+        if (timer >= spawnTimer)
+        {
+            SpawnGhost();
+            timer = 0f;
+        }
+    }
+
+    private void SpawnGhost()
+    {
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+
+        int currentTry = 0;
+
+        while (currentTry < spawnTry)
+        {
+            bool foundPosition = room.GenerateRandomPositionOnSurface(MRUK.SurfaceType.VERTICAL, minEdgeDistance, new LabelFilter(spwanLabels), out Vector3 position, out Vector3 normal);
+
+            if ((foundPosition))
+            {
+                Vector3 randomPositionNormalOffset = position + normal * normalOffset; // Offset slightly above the surface
+                randomPositionNormalOffset.y = 0;
+
+                GameObject ghost = Instantiate(ghostPrefab, randomPositionNormalOffset, Quaternion.identity);
+                ghost.transform.LookAt(transform.position);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Failed to find a valid position for ghost spawn after " + spawnTry + " attempts.");
+            currentTry++;
+        }
+
+
+
+    }
+}
